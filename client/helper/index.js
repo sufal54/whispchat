97095453bac4b80f4b4c5662c0824daf3895530a
sufal54 <145686201+sufal54@users.noreply.github.com>
2024-06-14 02:21:07 -0400

    Add files via upload

---
+++ client/helper/index.js
@@ -0,0 +1,15 @@
+export const female = [
+    "https://i.pinimg.com/474x/85/42/af/8542aff2a7c9a77413cf05850eb1ab4e.jpg",
+    "https://banner2.cleanpng.com/20180612/hv/kisspng-computer-icons-designer-avatar-5b207ebb279901.8233901115288562511622.jpg",
+    "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-user-cartoon-girl-avatar-image_1200112.jpg",
+    "https://png.pngtree.com/element_our/20190529/ourmid/pngtree-flat-user-pattern-round-avatar-pattern-image_1200096.jpg",
+    "https://png.pngtree.com/png-clipart/20190904/original/pngtree-user-cartoon-girl-avatar-png-image_4492903.jpg"
+];
+
+export const male = [
+    "https://e7.pngegg.com/pngimages/550/997/png-clipart-user-icon-foreigners-avatar-child-face.png",
+    "https://img.favpng.com/12/2/18/end-user-portable-network-graphics-vector-graphics-computer-icons-png-favpng-GUJJJM6GLdVVeuWCcMZdH2hcB.jpg",
+    "https://thumbs.dreamstime.com/b/man-hipster-avatar-cartoon-guy-black-hair-man-hipster-avatar-cartoon-guy-black-hair-flat-icon-blue-background-user-223717055.jpg",
+    "https://cdn-icons-png.flaticon.com/512/428/428573.png",
+    "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg"
+];
